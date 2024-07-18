8050d7a1c2d385603b9cc5cd103a799fe2acb2d3
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-07-19 03:54:16 +0530

    security group added

--- k8s_nodegroup.tf
+++ k8s_nodegroup.tf
@@ -1,3 +1,48 @@
+data "aws_security_group" "eks_control_plane_sg" {
+  filter {
+    name   = "tag:aws:eks:cluster-name"
+    values = [aws_eks_cluster.example.name]
+  }
+}
+
+resource "aws_security_group" "eks_worker_sg" {
+  name        = "eks_worker_sg"
+  description = "EKS Worker Nodes Security Group"
+  vpc_id      = aws_vpc.myvpc.id
+
+  ingress {
+    description      = "Allow pods to communicate with the cluster API Server"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    security_groups  = [data.aws_security_group.eks_control_plane_sg.id]
+  }
+
+  ingress {
+    description      = "Allow nodes to communicate with each other"
+    from_port        = 0
+    to_port          = 65535
+    protocol         = "-1"
+    cidr_blocks      = [prisub1a_cidr_block,prisub1b_cidr_block]
+  }
+
+  ingress {
+    description      = "Allow worker Kubelets and pods to receive communication from the cluster control plane"
+    from_port        = 1025
+    to_port          = 65535
+    protocol         = "tcp"
+    cidr_blocks      = [prisub1a_cidr_block,prisub1b_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+
 resource "aws_iam_role" "node" {
   name = "${var.cluster-name}-eks-node-role"
 
@@ -33,21 +78,26 @@ resource "aws_eks_node_group" "nodegroup" {
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = [aws_subnet.prisubnet1a.id,aws_subnet.prisubnet1b.id]
   instance_types = [var.instance_types]
+  
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 4
     min_size     = 1
   }
 
   update_config {
     max_unavailable = 1
   }
+  remote_access {
+    ec2_ssh_key = "ssm"
+    source_security_group_ids = [aws_security_group.eks_worker_sg.id]
+  }
 
   # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
   # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
     aws_iam_role_policy_attachment.amazon_eks_workr_node_policy,
-    aws_iam_role_policy_attachment.cluster-AmazonEKSClusterPolicy
+    aws_iam_role_policy_attachment.cluster-AmazonEKS_CNI_Policy
   ]
 }
 
