dca4ed0c38b62f007847e3fe255f45e38b6bca44
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-07-23 04:50:50 +0530

    security group added

--- k8s_nodegroup.tf
+++ k8s_nodegroup.tf
@@ -1,4 +1,3 @@
-
 data "aws_security_group" "eks_control_plane_sg" {
   filter {
     name   = "group-name"  # Key to filter by security group name
@@ -25,7 +24,7 @@ resource "aws_security_group" "eks_worker_sg" {
     from_port        = 0
     to_port          = 65535
     protocol         = "tcp"
-    cidr_blocks      = [var.prisub1a_cidr_block,var.prisub1b_cidr_block]
+    cidr_blocks      = [var.prisub1a_cidr_block, var.prisub1b_cidr_block]
   }
 
   ingress {
@@ -33,7 +32,7 @@ resource "aws_security_group" "eks_worker_sg" {
     from_port        = 1025
     to_port          = 65535
     protocol         = "tcp"
-    cidr_blocks      = [var.prisub1a_cidr_block,var.prisub1b_cidr_block]
+    cidr_blocks      = [var.prisub1a_cidr_block, var.prisub1b_cidr_block]
   }
 
   egress {
@@ -44,11 +43,10 @@ resource "aws_security_group" "eks_worker_sg" {
   }
 }
 
-
 resource "aws_iam_role" "node" {
-  name = "${var.cluster-name}-eks-node-role"
+  name = "${var.cluster_name}-eks-node-role"
 
-   assume_role_policy = jsonencode({
+  assume_role_policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
@@ -60,26 +58,25 @@ resource "aws_iam_role" "node" {
       }
     ]
   })
-
 }
-#attaching polices 
-resource "aws_iam_role_policy_attachment" "amazon_eks_workr_node_policy" {
+
+resource "aws_iam_role_policy_attachment" "amazon_eks_worker_node_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.node.name
+  role      = aws_iam_role.node.name
 }
 
-resource "aws_iam_role_policy_attachment" "cluster-AmazonEKS_CNI_Policy" {
+resource "aws_iam_role_policy_attachment" "amazon_eks_cni_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.node.name
+  role      = aws_iam_role.node.name
 }
 
 resource "aws_eks_node_group" "nodegroup" {
   cluster_name    = aws_eks_cluster.eks.name
-  version = var.eks_version
+  version         = var.eks_version
   node_group_name = "general"
   node_role_arn   = aws_iam_role.node.arn
-  subnet_ids      = [aws_subnet.prisubnet1a.id,aws_subnet.prisubnet1b.id]
-  instance_types = [var.instance_types]
+  subnet_ids      = [aws_subnet.prisubnet1a.id, aws_subnet.prisubnet1b.id]
+  instance_types  = [var.instance_types]
   
   scaling_config {
     desired_size = 2
@@ -90,16 +87,16 @@ resource "aws_eks_node_group" "nodegroup" {
   update_config {
     max_unavailable = 1
   }
+
   remote_access {
-    ec2_ssh_key = "ssm"
+    # Specify a valid SSH key pair name or remove if SSH access is not needed
+    ec2_ssh_key = "your-ssh-key-name"  
     source_security_group_ids = [aws_security_group.eks_worker_sg.id]
   }
 
-  # Ensure that IAM Role permissions are created before and deleted after EKS Node Group handling.
-  # Otherwise, EKS will not be able to properly delete EC2 Instances and Elastic Network Interfaces.
   depends_on = [
-    aws_iam_role_policy_attachment.amazon_eks_workr_node_policy,
-    aws_iam_role_policy_attachment.cluster-AmazonEKS_CNI_Policy
+    aws_iam_role.node,
+    aws_iam_role_policy_attachment.amazon_eks_worker_node_policy,
+    aws_iam_role_policy_attachment.amazon_eks_cni_policy
   ]
 }
-
