e037a4b357c431c960fea74fae4037be9db88ed0
prasannakumar <prasannakumarsinganamalla@gmail.com>
2024-07-23 09:35:34 +0530

    security group added

--- k8s_nodegroup.tf
+++ k8s_nodegroup.tf
@@ -78,6 +78,8 @@ resource "aws_eks_node_group" "nodegroup" {
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = [aws_subnet.prisubnet1a.id, aws_subnet.prisubnet1b.id]
   instance_types  = [var.instance_types]
+  ami_type = "AL2_x86_64"
+
   
   scaling_config {
     desired_size = 2
